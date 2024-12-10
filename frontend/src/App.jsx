1f13eedca834bb212106f00cbae195dac0148e56
YersiniaPestis899 <karuma1941@gmail.com>
2024-12-10 18:48:34 +0900

    act2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StoryGenerator from './components/StoryGenerator';
-import CharacterCreator from './components/CharacterCreator';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-100">
-        <Navigation />
         <div className="container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<StoryGenerator />} />
-            <Route path="/characters" element={<CharacterCreator />} />
           </Routes>
         </div>
       </div>
