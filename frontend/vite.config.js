c83a9c738f231e2250fe3ca4b9f03ec492a4891f
YersiniaPestis899 <karuma1941@gmail.com>
2024-12-23 06:34:14 +0900

    build error 5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,23 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': [
-            'react',
-            'react-dom',
-            'react-router-dom',
-            '@supabase/supabase-js'
-          ]
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000
+    outDir: 'dist'
   }
 })
